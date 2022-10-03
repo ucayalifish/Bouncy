b044ee5052eca854e7a3ed453db2fc81aa00fd48
Sergey Skorokhodov <ucayalyfish@yandex.ru>
2022-10-03 10:07:25 +0300

    Up to 2.13.7

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,34 @@ struct Ball {
     horiz_dir: HorizDir,
 }
 
+impl Ball {
+    fn bounce(&mut self, frame: &Frame) {
+        if self.x == 0 {
+            self.horiz_dir = HorizDir::Right;
+        } else if self.x == frame.width - 1 {
+            self.horiz_dir = HorizDir::Left;
+        }
+
+        if self.y == 0 {
+            self.vert_dir = VertDir::Down;
+        } else if self.y == frame.height - 1 {
+            self.vert_dir = VertDir::Up;
+        }
+    }
+
+    fn mv(&mut self) {
+        match self.horiz_dir {
+            HorizDir::Left => self.x -= 1,
+            HorizDir::Right => self.x += 1,
+        }
+
+        match self.vert_dir {
+            VertDir::Up => self.y -= 1,
+            VertDir::Down => self.y += 1,
+        }
+    }
+}
+
 struct Frame {
     width: u32,
     height: u32,
@@ -39,6 +67,11 @@ impl Game {
                 horiz_dir: HorizDir::Left,
             },
         }
+    }
+
+    fn step(&mut self) {
+        self.ball.bounce(&self.frame);
+        self.ball.mv();
     }
 }
 
