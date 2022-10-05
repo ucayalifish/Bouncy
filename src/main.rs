48a7a1db90630de159eb6b868e4ef7c55c767960
Sergey Skorokhodov <ucayalyfish@yandex.ru>
2022-10-05 22:03:27 +0300

    Up to 2.13.13

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,109 @@
+use std::fmt::{Display, Formatter};
+
 enum VertDir {
-    Up,
-    Down,
+  Up,
+  Down,
 }
 
 enum HorizDir {
-    Left,
-    Right,
+  Left,
+  Right,
 }
 
 struct Ball {
-    x: u32,
-    y: u32,
-    vert_dir: VertDir,
-    horiz_dir: HorizDir,
+  x: u32,
+  y: u32,
+  vert_dir: VertDir,
+  horiz_dir: HorizDir,
 }
 
 impl Ball {
-    fn bounce(&mut self, frame: &Frame) {
-        if self.x == 0 {
-            self.horiz_dir = HorizDir::Right;
-        } else if self.x == frame.width - 1 {
-            self.horiz_dir = HorizDir::Left;
-        }
-
-        if self.y == 0 {
-            self.vert_dir = VertDir::Down;
-        } else if self.y == frame.height - 1 {
-            self.vert_dir = VertDir::Up;
-        }
+  fn bounce(&mut self, frame: &Frame) {
+    if self.x == 0 {
+      self.horiz_dir = HorizDir::Right;
+    } else if self.x == frame.width - 1 {
+      self.horiz_dir = HorizDir::Left;
     }
 
-    fn mv(&mut self) {
-        match self.horiz_dir {
-            HorizDir::Left => self.x -= 1,
-            HorizDir::Right => self.x += 1,
-        }
-
-        match self.vert_dir {
-            VertDir::Up => self.y -= 1,
-            VertDir::Down => self.y += 1,
-        }
+    if self.y == 0 {
+      self.vert_dir = VertDir::Down;
+    } else if self.y == frame.height - 1 {
+      self.vert_dir = VertDir::Up;
     }
+  }
+
+  fn mv(&mut self) {
+    match self.horiz_dir {
+      HorizDir::Left => self.x -= 1,
+      HorizDir::Right => self.x += 1,
+    }
+
+    match self.vert_dir {
+      VertDir::Up => self.y -= 1,
+      VertDir::Down => self.y += 1,
+    }
+  }
 }
 
 struct Frame {
-    width: u32,
-    height: u32,
+  width: u32,
+  height: u32,
 }
 
 struct Game {
-    frame: Frame,
-    ball: Ball,
+  frame: Frame,
+  ball: Ball,
 }
 
 impl Game {
-    fn new() -> Game {
-        Game {
-            frame: Frame {
-                width: 60,
-                height: 30,
-            },
-            ball: Ball {
-                x: 2,
-                y: 4,
-                vert_dir: VertDir::Up,
-                horiz_dir: HorizDir::Left,
-            },
-        }
+  fn new() -> Game {
+    Game {
+      frame: Frame {
+        width: 60,
+        height: 30,
+      },
+      ball: Ball {
+        x: 2,
+        y: 4,
+        vert_dir: VertDir::Up,
+        horiz_dir: HorizDir::Left,
+      },
     }
+  }
 
-    fn step(&mut self) {
-        self.ball.bounce(&self.frame);
-        self.ball.mv();
+  fn step(&mut self) {
+    self.ball.bounce(&self.frame);
+    self.ball.mv();
+  }
+}
+
+impl Display for Game {
+  fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
+    let mut top_bottom = |fmt: &mut Formatter| {
+      write!(fmt, "+");
+      for _ in 0..self.frame.width {
+        write!(fmt, "-");
+      }
+      write!(fmt, "+\n")
+    };
+
+    top_bottom(fmt);
+    for row in 0..self.frame.height {
+      write!(fmt, "|");
+      for column in 0..self.frame.width {
+        let c = if row == self.ball.y && column == self.ball.x {
+          'o'
+        } else {
+          ' '
+        };
+        write!(fmt, "{}", c);
+      }
+      write!(fmt, "|\n");
     }
+    top_bottom(fmt)
+  }
 }
 
 fn main() {
-    println!("Hello, world!");
+  println!("{}", Game::new());
 }
