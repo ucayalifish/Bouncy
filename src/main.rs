2245a8d20353f51b590213fadf6a57ad68a77574
Sergey Skorokhodov <ucayalyfish@yandex.ru>
2022-10-06 21:12:49 +0300

    Chapter 2 completed

--- src/main.rs
+++ src/main.rs
@@ -57,18 +57,17 @@ struct Game {
 
 impl Game {
   fn new() -> Game {
-    Game {
-      frame: Frame {
-        width: 60,
-        height: 30,
-      },
-      ball: Ball {
-        x: 2,
-        y: 4,
-        vert_dir: VertDir::Up,
-        horiz_dir: HorizDir::Left,
-      },
-    }
+    let frame = Frame {
+      width: 60,
+      height: 30,
+    };
+    let ball = Ball {
+      x: 2,
+      y: 4,
+      vert_dir: VertDir::Up,
+      horiz_dir: HorizDir::Left,
+    };
+    Game {frame, ball}
   }
 
   fn step(&mut self) {
@@ -79,31 +78,37 @@ impl Game {
 
 impl Display for Game {
   fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
-    let mut top_bottom = |fmt: &mut Formatter| {
-      write!(fmt, "+");
+    let top_bottom = |fmt: &mut Formatter| {
+      write!(fmt, "+")?;
       for _ in 0..self.frame.width {
-        write!(fmt, "-");
+        write!(fmt, "-")?;
       }
       write!(fmt, "+\n")
     };
 
-    top_bottom(fmt);
+    top_bottom(fmt)?;
     for row in 0..self.frame.height {
-      write!(fmt, "|");
+      write!(fmt, "|")?;
       for column in 0..self.frame.width {
         let c = if row == self.ball.y && column == self.ball.x {
           'o'
         } else {
           ' '
         };
-        write!(fmt, "{}", c);
+        write!(fmt, "{}", c)?;
       }
-      write!(fmt, "|\n");
+      write!(fmt, "|\n")?;
     }
     top_bottom(fmt)
   }
 }
 
 fn main() {
-  println!("{}", Game::new());
+  let mut game = Game::new();
+  let sleep_duration = std::time::Duration::from_millis(33);
+  loop {
+    println!("{}", game);
+    game.step();
+    std::thread::sleep(sleep_duration);
+  }
 }
